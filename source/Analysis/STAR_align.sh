dfe1057f255a363d090b74850c43503eb2f65ecb
John Reddy Peasari <55759107+jpeasari@users.noreply.github.com>
2021-01-25 00:22:14 -0600

    Create STAR_align.sh

---
+++ source/Analysis/STAR_align.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+mkdir ../../star_align
+cd ../../star_align
+
+for sample in ../../Data/raw/*.fastq;
+out = ${i}
+do
+  STAR --runMode alignReads \
+        --runThreadN 20 \
+        --genomeDir star_index \
+        --outSAMtype BAM  SortedByCoordinate \
+        --outSAMunmapped Within  \
+        --outSAMattributes Standard \
+        --outFileNamePrefix ../../star_align/out  \
+        --readFilesIn ${sample} \
+        --limitBAMsortRAM 10000000000
+done
