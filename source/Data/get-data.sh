ff461379bcfb2bfa9c5bf28c147a59d82fb7da21
John Reddy Peasari <55759107+jpeasari@users.noreply.github.com>
2021-01-24 23:51:49 -0600

    Update get-data.sh

--- source/Data/get-data.sh
+++ source/Data/get-data.sh
@@ -2,3 +2,16 @@
 mkdir ../raw
 cd ..
 files = "SRR106657,SRR106658,SRR106659,SRR106660"
+for i in $files;
+do
+  fastq-dump $i
+ done
+ 
+wget https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/146/045/GCF_000146045.2_R64/GCF_000146045.2_R64_genomic.fna.gz
+wget https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/146/045/GCF_000146045.2_R64/GCF_000146045.2_R64_rna.fna.gz
+wget ftp://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/146/045/GCF_000146045.2_R64/GCF_000146045.2_R64_genomic.gff.gz
+wget ftp://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/146/045/GCF_000146045.2_R64/GCF_000146045.2_R64_genomic.gff.gz
+
+gunzip *.gz
+
+
