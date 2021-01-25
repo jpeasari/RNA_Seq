3267deef7acd7adb4a74b7916e2a85c4d70463c8
John Reddy Peasari <55759107+jpeasari@users.noreply.github.com>
2021-01-25 00:30:05 -0600

    Update STAR-Index.sh

--- source/Analysis/STAR-Index.sh
+++ source/Analysis/STAR-Index.sh
@@ -7,7 +7,7 @@ STAR	--runThreadN 12  \
        --runMode genomeGenerate \
        --genomeDir genome_index \
        --genomeFastaFiles ../../Data/raw/GCF_000146045.2_R64_genomic.fna \
-       --sjdbGTFfile ../../Data/raw/GCF_000146045.2_R64_genomic.gff \
+       --sjdbGTFfile ../../Data/raw/GCF_000146045.2_R64_genomic.gtf \
        --sjdbOverhang 49 \
        --genomeSAindexNbases 10  \
        --limitGenomeGenerateRAM 10000000000
